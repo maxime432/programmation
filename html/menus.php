fadbb1f1291755d7af57c0b272fb5f0947916c7a
maxime432 <maximebeaudouin@yahoo.fr>
2017-01-25 21:10:39 +0100

    Ajout premiers fichiers

---
+++ html/menus.php
@@ -0,0 +1,16 @@
+<nav>
+	<ul>
+		<li><a href="#">Accueil</a></li>
+		<li><a href="#">Mon progil</a></li>
+		<li><a href="#">Messagerie</a></li>
+		<li><a href="#">Membres</a></li>
+		<li><a href="#">Agenda</a></li>
+		<li><a href="#">Actualites</a></li>
+		<li><a href="#">Formation</a></li>
+		<li><a href="#">Biens</a></li>
+		<li><a href="#">Services</a></li>
+		<li><a href="#">Finance</a></li>
+		<li><a href="#">Administration</a></li>
+		<li><a href="#">Deconnexion</a></li>
+	</ul>
+</nav>
