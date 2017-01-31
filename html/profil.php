1014a920fd4b66a77c44fecd86151e06ff0c3f2b
maxime432 <maximebeaudouin@yahoo.fr>
2017-01-31 23:03:06 +0100

    Connexion et premiere requete

--- html/profil.php
+++ html/profil.php
@@ -1,14 +1,14 @@
 <?php
-	//include("include/connexion.php") ;
+	include("include/connexion.php") ;
 
-	//$requete_select = $bdd->prepare('SELECT nom, prenom, nom_utilisateur, adresse_mail FROM membres LIMIT 1');
-	//$requete_select->execute() ;
-	//$donnees_profil = $requete_select.fetch() ;
+	$requete_select = $bdd->prepare('SELECT nom, prenom, nom_utilisateur, adresse_mail FROM membres LIMIT 1');
+	$requete_select->execute() ;
+	$donnees_profil = $requete_select->fetch() ;
 
-	$name = 'Beaudouin' ; //$donnees_profil['nom'] ;
-	$firstname = 'Max' ; //$donnees_profil['prenom'] ;
-	$nickname = 'Maxime432' ; // $donnees_profil['nom_utilisateur'];
-	$mail = 'maxime432@hotmail.fr' ; // $donnees_profil['adresse_mail'] ;
+	$name = $donnees_profil['nom'] ;
+	$firstname = $donnees_profil['prenom'] ;
+	$nickname = $donnees_profil['nom_utilisateur'];
+	$mail = $donnees_profil['adresse_mail'] ;
 ?>
 <!DOCTYPE html>
 <html>
@@ -24,13 +24,13 @@
 
 		<section>
 			<p>
-				<label for="name"> <?php echo $name . " " . ; echo $firstname ?>	</label><br />
+				<label for="name"> <?php echo $name . " " ; echo $firstname ?>	</label><br />
 				<label for="nickname"> <?php echo $nickname; ?></label><br />
 				<label for="mail"> <?php echo $mail; ?></label><br />
 			</p>
 
 			<form method="POST" action="edit_profil.php">
-				<button type="submit" value="Editer"/>
+				<button type="submit" value="Editer" />
 			</form>
 		</section>
 
