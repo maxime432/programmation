ab21d83a6289dc32d79d3fcfd065e1ed106fb4c7
maxime432 <maximebeaudouin@yahoo.fr>
2017-02-21 21:01:25 +0100

    Premier test Bootstrap

--- html/menus.php
+++ html/menus.php
@@ -14,3 +14,36 @@
 		<li><a href="#">Deconnexion</a></li>
 	</ul>
 </nav>
+
+<div class="container">
+	<div class="col-lg-2">
+	    <ul class="list-group">
+	      <li class="list-group-item">Accueil</li>
+	      <li class="list-group-item">Mon profil</li>
+	      <li class="list-group-item">Messagerie</li>
+	      <li class="list-group-item">Membres</li>
+	      <li class="list-group-item">Agenda</li>
+	      <li class="list-group-item">Actualités</li>
+	      <li class="list-group-item">Formations</li>
+	      <li class="list-group-item">Biens</li>
+	      <li class="list-group-item">Services</li>
+	      <li class="list-group-item">Finance</li>
+	      <li class="list-group-item">Administration</li>
+	      <li class="list-group-item">Déconnexion</li>
+	    </ul>
+	</div>
+    <section class="row">
+        <div class="col-lg-2">Accueil</div>
+        <div class="col-lg-2">Mon profil</div>
+        <div class="col-lg-2">Messagerie</div>
+        <div class="col-lg-2">Membres</div>
+        <div class="col-lg-2">Agenda</div>
+        <div class="col-lg-2">Actualites</div>
+        <div class="col-lg-2">Formations</div>
+        <div class="col-lg-2">Biens</div>
+        <div class="col-lg-2">Services</div>
+        <div class="col-lg-2">Finance</div>
+        <div class="col-lg-2">Administration</div>
+        <div class="col-lg-2">Deconnexion</div>
+    </section>
+</div>
