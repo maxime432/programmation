3b20a5b39817cddba83c255d62d08820669aaf51
maxime432 <maximebeaudouin@yahoo.fr>
2017-02-21 21:43:02 +0100

    Suppression des différents menus

--- html/menus.php
+++ html/menus.php
@@ -1,19 +1,4 @@
-<nav>
-	<ul>
-		<li><a href="#">Accueil</a></li>
-		<li><a href="#">Mon progil</a></li>
-		<li><a href="#">Messagerie</a></li>
-		<li><a href="#">Membres</a></li>
-		<li><a href="#">Agenda</a></li>
-		<li><a href="#">Actualites</a></li>
-		<li><a href="#">Formation</a></li>
-		<li><a href="#">Biens</a></li>
-		<li><a href="#">Services</a></li>
-		<li><a href="#">Finance</a></li>
-		<li><a href="#">Administration</a></li>
-		<li><a href="#">Deconnexion</a></li>
-	</ul>
-</nav>
+
 
 <div class="container">
 	<div class="col-lg-2">
@@ -32,18 +17,4 @@
 	      <li class="list-group-item">Déconnexion</li>
 	    </ul>
 	</div>
-    <section class="row">
-        <div class="col-lg-2">Accueil</div>
-        <div class="col-lg-2">Mon profil</div>
-        <div class="col-lg-2">Messagerie</div>
-        <div class="col-lg-2">Membres</div>
-        <div class="col-lg-2">Agenda</div>
-        <div class="col-lg-2">Actualites</div>
-        <div class="col-lg-2">Formations</div>
-        <div class="col-lg-2">Biens</div>
-        <div class="col-lg-2">Services</div>
-        <div class="col-lg-2">Finance</div>
-        <div class="col-lg-2">Administration</div>
-        <div class="col-lg-2">Deconnexion</div>
-    </section>
 </div>
