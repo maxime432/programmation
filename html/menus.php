884f10cad7f2b8e6c9002c6c95170cdd1c263ba7
maxime432 <maximebeaudouin@yahoo.fr>
2017-02-21 21:50:12 +0100

    Mise en place des couleurs sur le menu

--- html/menus.php
+++ html/menus.php
@@ -3,18 +3,18 @@
 <div class="container">
 	<div class="col-lg-2">
 	    <ul class="list-group">
-	      <li class="list-group-item">Accueil</li>
-	      <li class="list-group-item">Mon profil</li>
-	      <li class="list-group-item">Messagerie</li>
-	      <li class="list-group-item">Membres</li>
-	      <li class="list-group-item">Agenda</li>
-	      <li class="list-group-item">Actualités</li>
-	      <li class="list-group-item">Formations</li>
-	      <li class="list-group-item">Biens</li>
-	      <li class="list-group-item">Services</li>
-	      <li class="list-group-item">Finance</li>
-	      <li class="list-group-item">Administration</li>
-	      <li class="list-group-item">Déconnexion</li>
+	      <li class="list-group-item list-group-item-success">Accueil</li>
+	      <li class="list-group-item list-group-item-info">Mon profil</li>
+	      <li class="list-group-item list-group-item-info">Messagerie</li>
+	      <li class="list-group-item list-group-item-info">Membres</li>
+	      <li class="list-group-item list-group-item-info">Agenda</li>
+	      <li class="list-group-item list-group-item-info">Actualités</li>
+	      <li class="list-group-item list-group-item-info">Formations</li>
+	      <li class="list-group-item list-group-item-info">Biens</li>
+	      <li class="list-group-item list-group-item-info">Services</li>
+	      <li class="list-group-item list-group-item-info">Finance</li>
+	      <li class="list-group-item list-group-item-info">Administration</li>
+	      <li class="list-group-item list-group-item-info">Déconnexion</li>
 	    </ul>
 	</div>
 </div>
