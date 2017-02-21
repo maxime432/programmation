e88fabc68670835c21bb6a5d3125c9133bacf024
maxime432 <maximebeaudouin@yahoo.fr>
2017-02-21 22:04:21 +0100

    Modification mise en page

--- html/index.php
+++ html/index.php
@@ -8,53 +8,45 @@
     </head>
         
     <body>
-      <div id="bloc_page">
-        <header>
-          <!--<div id="logo">
-	       <img src="images/logo.jpg" alt="Logo d'ADEPT" />    
-          </div>-->
+      <div class="container">
+        <header class="row">
+            <div class="col-md-12">
+                <div id="logo">
+                   <img src="images/logo.jpg" alt="Logo d'ADEPT" />    
+                </div>
+            </div>
         </header>
             
-        <div class="col-md-12">
-            <div id="logo">
-               <img src="images/logo.jpg" alt="Logo d'ADEPT" />    
-            </div>
-        </div>
+        
 
-
-        <div class="container">
-            <div class="col-md-2">
-                <?php include("menus.php"); ?>
-            </div>
-            <div class="col-md-10">
-                <div id="titre_page">
-                  <h1>Bienvenu chez ADEPT </h1>
-                  <h2>Association de la Découverte, de l'Echange et du ParTage</h2>
-                </div>
-                <section>
-                  <article>
-                    <h3>Création de l'association :</h3>
-                    <p>ADEPT est née le 6 juillet 2016. Voulant faire découvrir et partager leurs passions au plus grand nombre, sept amis se sont regroupé et ont décidé de créer une dynamique autour d'activités sportives.
-                    </p>
-                    <p>Attirés vers la randonnée, le vélo de montagne et la spéléologie, c'est vers ces activités qu'ils se dirigent au départ. Ils organisent régulièrement des initiations et sorties en Alsace, permettant la découverte de la richesse naturelle de cette région.
-                    </p>
-                  </article>
-                </section>
-
-                <div id="image_accueil">
-                    <img src="images/images_accueil.jpeg" alt="Image d'une cavité" />
-                </div>  
-            </div>
-        </div>
-
-	
-    	
-
-    	
 
         
-        <footer>
+        <div class="col-md-2">
+            <?php include("menus.php"); ?>
+        </div>
+        <div class="col-md-10">
+            <div id="titre_page">
+              <h1>Bienvenu chez ADEPT </h1>
+              <h2>Association de la Découverte, de l'Echange et du ParTage</h2>
+            </div>
+            <section>
+              <article>
+                <h3>Création de l'association :</h3>
+                <p>ADEPT est née le 6 juillet 2016. Voulant faire découvrir et partager leurs passions au plus grand nombre, sept amis se sont regroupé et ont décidé de créer une dynamique autour d'activités sportives.
+                </p>
+                <p>Attirés vers la randonnée, le vélo de montagne et la spéléologie, c'est vers ces activités qu'ils se dirigent au départ. Ils organisent régulièrement des initiations et sorties en Alsace, permettant la découverte de la richesse naturelle de cette région.
+                </p>
+              </article>
+            </section>
+
+            <div id="image_accueil">
+                <img src="images/images_accueil.jpeg" alt="Image d'une cavité" />
+            </div>  
+        </div>
+
+        <footer class="row">
         </footer>
+        
        </div>
     </body>
 </html>
