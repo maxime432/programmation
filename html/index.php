eab4b6c6382779313ba8b68d9be2441bda842546
maxime432 <maximebeaudouin@yahoo.fr>
2017-02-21 20:47:17 +0100

    Ajout de bootstrap

--- html/index.php
+++ html/index.php
@@ -10,32 +10,32 @@
       <div id="bloc_page">
         <header>
           <div id="logo">
-	    <img src="images/logo.jpg" alt="Logo d'ADEPT" />    
+	       <img src="images/logo.jpg" alt="Logo d'ADEPT" />    
           </div>
         </header>
             
         <?php include("menus.php"); ?>
 	
-	<div id="titre_page">
-	  <h1>Bienvenu chez ADEPT </h1>
-	  <h2>Association de la Découverte, de l'Echange et du ParTage</h2>
-	</div>
+    	<div id="titre_page">
+    	  <h1>Bienvenu chez ADEPT </h1>
+    	  <h2>Association de la Découverte, de l'Echange et du ParTage</h2>
+    	</div>
 
-	<section>
-	  <article>
-	    <h3>Création de l'association :</h3>
-	    <p>ADEPT est née le 6 juillet 2016. Voulant faire découvrir et partager leurs passions au plus grand nombre, sept amis se sont regroupé et ont décidé de créer une dynamique autour d'activités sportives.
-	    </p>
-	    <p>Attirés vers la randonnée, le vélo de montagne et la spéléologie, c'est vers ces activités qu'ils se dirigent au départ. Ils organisent régulièrement des initiations et sorties en Alsace, permettant la découverte de la richesse naturelle de cette région.
-	    </p>
-	  </article>
-	</section>
+    	<section>
+    	  <article>
+    	    <h3>Création de l'association :</h3>
+    	    <p>ADEPT est née le 6 juillet 2016. Voulant faire découvrir et partager leurs passions au plus grand nombre, sept amis se sont regroupé et ont décidé de créer une dynamique autour d'activités sportives.
+    	    </p>
+    	    <p>Attirés vers la randonnée, le vélo de montagne et la spéléologie, c'est vers ces activités qu'ils se dirigent au départ. Ils organisent régulièrement des initiations et sorties en Alsace, permettant la découverte de la richesse naturelle de cette région.
+    	    </p>
+    	  </article>
+    	</section>
 
         <div id="image_accueil">
-	  <img src="images/images_accueil.jpeg" alt="Image d'une cavité" />
+    	  <img src="images/images_accueil.jpeg" alt="Image d'une cavité" />
         </div>
         <footer>
         </footer>
-      </div>
+       </div>
     </body>
 </html>
